aaff82b6093355cd6b396bc9bf50311a919ff40e
RandomEC <97635743+RandomEC@users.noreply.github.com>
2022-02-16 13:12:05 -0500

    Update characters.py
    Added damroll strength modifiers.

--- typeclasses/characters.py
+++ typeclasses/characters.py
@@ -234,7 +234,7 @@ class Character(DefaultCharacter):
                         multiplier = 1
                     modifier = modifier - (equipment.db.armor*multiplier)
 
-                    dexterity = self.get_modified_attribute(self, "dexterity")
+                    dexterity = self.dexterity
 
                     if dexterity < 1:
                         modifier += 60
@@ -279,7 +279,7 @@ class Character(DefaultCharacter):
 
                 if attribute_name == "hitroll":
 
-                    strength = self.get_modified_attribute(self, "strength")
+                    strength = self.strength
 
                     if strength < 2:
                         modifier += -5
@@ -307,6 +307,39 @@ class Character(DefaultCharacter):
                         modifier += 8
                     elif strength < 26:
                         modifier += 10
+                    
+                if attribute_name == "damroll":
+                    strength = self.strength
+
+                    if strength < 2:
+                        modifier += -4
+                    elif strength < 3:
+                        modifier += -2
+                    elif strength < 6:
+                        modifier += -1
+                    elif strength < 14:
+                        modifier += 0
+                    elif strength < 16:
+                        modifier += 1
+                    elif strength < 17:
+                        modifier += 2
+                    elif strength < 18:
+                        modifier += 3
+                    elif strength < 19:
+                        modifier += 4
+                    elif strength < 20:
+                        modifier += 5
+                    elif strength < 21:
+                        modifier += 6
+                    elif strength < 23:
+                        modifier += 7
+                    elif strength < 24:
+                        modifier += 8
+                    elif strength < 25:
+                        modifier += 10
+                    elif strength < 26:
+                        modifier += 12
+                    
 
         # assemble modifier with base stat, first for the "trainable" stats, which have an extra modifier for that
 
