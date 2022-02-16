3276c1f955ae8b481651deab5fa11cdc5be033f5
RandomEC <97635743+RandomEC@users.noreply.github.com>
2022-02-16 12:33:56 -0500

    Update rules_combat.py
    Created do_attack, do_single_hit and modified do_damage

--- world/rules_combat.py
+++ world/rules_combat.py
@@ -1,5 +1,71 @@
 import random
+from world import rules_race
 
+def do_attack(attacker, victim):
+    hit = True
+    damage = 1
+    
+    if hit:
+        victim.take_damage(damage)
+        return "%s does %d damage to %s" % (attacker.key.capitalize(), damage, victim.key)
+    else:
+        return "%s misses %s" % (attacker.key.capitalize(), victim.key)
+
+def do_damage(attacker, weapon_slot):
+    
+    if "mobile" in attacker.tags.all():
+        damage_low = int(attacker.db.level*3/4)
+        damage_high = int(attacker.db.level*3/2)
+        
+        # Damage is a random number between high damage and low damage.
+        damage = random.randint(damage_low, damage_high)
+        
+        # If mobile is wielding a weapon, they get a 50% bonus.
+        if attacker.db.eq_slots["wielded, primary"]:
+            damage *= 1.5
+
+        # Get a bonus to damage from damroll.
+        dam_bonus = attacker.damroll
+        
+        damage += dam_bonus
+
+    else:
+        if attacker.db.eq_slots[weapon_slot]:
+            weapon = attacker.db.eq_slots[weapon_slot]
+            damage = random.randint(weapon.db.damage_low, weapon.db.damage_high)
+        
+            dam_bonus = attacker.damroll
+            
+            # Subtract out damroll from weapon not being used in the attack, if
+            # more than one.
+            if weapon_slot == "wielded, primary":
+                if attacker.db.eq_slots["wielded, secondary"]:
+                    eq = attacker.db.eq_slots["wielded, secondary"]
+                    dam_bonus -= eq.db.stat_modifiers["damroll"]
+            else: 
+                eq = attacker.db.eq_slots["wielded, secondary"]
+                dam_bonus -= eq.db.stat_modifiers["damroll"]
+                        
+            damage += dam_bonus
+            
+        else:
+            damage = random.randint(1, 2)*attacker.size
+    
+            # Get a bonus to damage from damroll, since we got here, there is no
+            # weapon to worry about.
+            dam_bonus = attacker.damroll
+            
+            damage += dam_bonus
+    
+    return damage
+
+def do_single_hit(attacker, victim):
+    hit_chance = get_hit_chance(attacker, victim)
+    if random.randint(1,100) <= hit_chance:
+        return True
+    else:
+        return False
+    
 def get_ac(combatant): # MOVE THIS TO CHARACTER CLASS FETCH COMMAND
     if combatant.db.type == "mobile":
         # On CA, mobile armor was interpolated between 100 and -400
@@ -18,59 +84,6 @@ def get_avoidskill(victim):
         return avoidskill
     else:
         return 1
-
-def get_damage(attacker, weapon_slot):
-    if attacker.db.character_type == "mobile":
-        damage_low = int(attacker.db.level*3/4)
-        damage_high = int(attacker.dg.level*3/2)
-        
-        damage = random.randint(damage_low, damage_high)
-        
-        if attacker.db.eq_slots["wielded, primary"]:
-            damage *= 1.5
- 
-    else:
-        if attacker.db.eq_slots[weapon_slot]:
-            weapon = attacker.db.eq_slots[weapon_slot]
-            damage = random.randint(weapon.db.damage_low, weapon.db.damage_high)
-        else:
-            damage = random.randint(1, 2)*attacker.db.size   # Need to create a method to get character size from race.
-    
-    # Get damroll
-    # Subtract damroll for other hand weapon, or both weapons, if can't use weapon
-
-def get_hit_chance(attacker, victim):
-    hit_chance = int(100 * (get_hitskill(attacker, victim) + attacker.db.level - victim.db.level)/(get_hitskill(attacker, victim) + get_avoidskill(victim)))
-    if hit_chance > 95:
-        return 95
-    elif hit_chance < 5:
-        return 5
-    else:
-        return hit_chance
-
-def get_hitskill(attacker, victim):
-    # Make sure that hitroll does not include hitroll from weapon if can't wield it.
-    hitskill = get_warskill(attacker) + attacker.get_modified_attribute("hitroll") + get_race_hitbonus(attacker, victim) + 10*(attacker.get_modified_attribute("dexterity") - 10)
-    if hitskill > 1:
-        return hitskill
-    else:
-        return 1
-
-def get_race_hitbonus(attacker, victim):
-    hitbonus = victim.size - attacker.size
-    return hitbonus
-
-def get_warskill(combatant):
-    if combatant.db.type == "mobile":
-        warskill_factor = combatant.db.level/101
-        warskill = int(120*warskill_factor)
-        return warskill
-    else:
-        # Will eventually deal with variable player warskill based on
-        # class-type skills learned.
-        warskill_factor = combatant.db.level/101
-        warskill = int(120*warskill_factor)
-        return warskill
 
 def get_damagestring(attacker, victim, damage):
     if "mobile" in victim.tags.all():
@@ -228,5 +241,39 @@ def get_damagestring(attacker, victim, damage):
         else:
             damagestring = "**>*>*>*VAPORIZES*<*<*<**"
             
+def get_hit_chance(attacker, victim):
+    hit_chance = int(100 * (get_hitskill(attacker, victim) + attacker.db.level - victim.db.level)/(get_hitskill(attacker, victim) + get_avoidskill(victim)))
+    if hit_chance > 95:
+        return 95
+    elif hit_chance < 5:
+        return 5
+    else:
+        return hit_chance
+
+def get_hitskill(attacker, victim):
+    # Make sure that hitroll does not include hitroll from weapon if can't wield it.
+    hitskill = get_warskill(attacker) + attacker.get_modified_attribute("hitroll") + get_race_hitbonus(attacker, victim) + 10*(attacker.get_modified_attribute("dexterity") - 10)
+    if hitskill > 1:
+        return hitskill
+    else:
+        return 1
+
+def get_race_hitbonus(attacker, victim):
+    hitbonus = victim.size - attacker.size
+    return hitbonus
+
+def get_warskill(combatant):
+    if combatant.db.type == "mobile":
+        warskill_factor = combatant.db.level/101
+        warskill = int(120*warskill_factor)
+        return warskill
+    else:
+        # Will eventually deal with variable player warskill based on
+        # class-type skills learned.
+        warskill_factor = combatant.db.level/101
+        warskill = int(120*warskill_factor)
+        return warskill
+
+
             
             
