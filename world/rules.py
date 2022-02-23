b5120deb95498a3f393c40bce9a366c48d8a5b46
RandomEC <97635743+RandomEC@users.noreply.github.com>
2022-02-23 11:46:13 -0500

    Update rules.py
    Added special function support to exp calculation

--- world/rules.py
+++ world/rules.py
@@ -45,19 +45,28 @@ def calculate_experience(mobile):
     experience = level_xp + hp_xp - ac_xp + damage_xp + hitroll_xp
 
     if mobile.get_affect_status("sanctuary"):
-
         experience *= 1.5
 
     if mobile.get_affect_status("flameshield"):
-
         experience *= 1.4
     
     if mobile.db.eq_slots["wielded, primary"]:
-        
         experience *= 1.25
     
     if mobile.db.eq_slots["wielded, secondary"]:
-        
         experience *= 1.2
 
+    if mobile.db.special_function:
+        if ("breath_any" or "cast_psionicist" or "cast_undead" or "breath_gas" or "cast_mage") in mobile.db.special_function:
+            experience *= 1.33
+
+        elif ("breath_fire" or "breath_frost" or "breath_acid" or "breath_lightning" or "cast_cleric" or "cast_judge" or "cast_ghost") in mobile.db.special_function:
+            experience *= 1.2
+
+        elif ("poison" or "thief") in mobile.db.special_function:
+            experience *= 1.05
+            
+        elif "cast_adept" in mobile.db.special_function:
+            experience *= 0.5
+           
     return experience
