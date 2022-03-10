a341edddee5ff4a4560a1a00d5c223fb8fc25bf5
RandomEC <97635743+RandomEC@users.noreply.github.com>
2022-03-10 11:29:58 -0500

    Create rules_skills.py
    Created enhanced damage.

---
+++ world/rules_skills.py
@@ -0,0 +1,14 @@
+"""
+This rules file is to handle anything related to skills and/or spells
+"""
+
+def get_skill(skill_name):
+    skills = {
+        "enhanced damage": {
+            "classes" : {
+                "warrior": 3
+                "ranger": 13
+                "paladin": 11
+                }
+            }
+        }
