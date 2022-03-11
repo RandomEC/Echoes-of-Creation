dcf9f6c733f71f7276d5b32239fb3e810e3118ee
RandomEC <brad.maynes@gmail.com>
2022-03-10 19:07:35 -0500

    Character creation troubleshooting.

--- typeclasses/scripts_character_creation.py
+++ typeclasses/scripts_character_creation.py
@@ -581,18 +581,22 @@ class ChooseNormalScript(Script):
         def make_object(location, equipped, reset_object):
 
             location.msg("In make_object 1")
+            location.msg(reset_object)
 
             # First, search for all objects of that type and pull out
             # any that are at "None".
             object_candidates = search.search_object(reset_object)
 
             location.msg("In make_object 2")
+            location.msg(object_candidates)
 
             for object in object_candidates:
+                location.msg(object.location)
                 if not object.location:
                     new_object = object
 
             location.msg("In make_object 3")
+            location.msg(new_object)
 
             # If it is not in "None", find the existing object in the world
             # and copy it.
@@ -631,9 +635,9 @@ class ChooseNormalScript(Script):
             if equipped:
                 if not new_object.db.equipped:
                     if new_object.db.item_type == "weapon":
-                        new_object.wield_to(self)
+                        new_object.wield_to(location)
                     else:
-                        new_object.wear_to(self)
+                        new_object.wear_to(location)
 
             return new_object
 
@@ -658,6 +662,8 @@ class ChooseNormalScript(Script):
                 pot_pie = make_object(newbie_bag, False, "o3009")
                 brownie = make_object(newbie_bag, False, "o1117")
                 brownie = make_object(newbie_bag, False, "o1117")
+
+                caller.msg("Back to confirmation")
 
                 destination = search.object_search("r3700")
                 home = search.object_search("r101")
