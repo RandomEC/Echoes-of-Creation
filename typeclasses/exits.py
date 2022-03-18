103198c92f5746f8e6541130a505787dc97e3e77
RandomEC <97635743+RandomEC@users.noreply.github.com>
2022-03-18 08:53:10 -0400

    Update exits.py
    Added cost of moving.

--- typeclasses/exits.py
+++ typeclasses/exits.py
@@ -64,3 +64,65 @@ class Exit(DefaultExit):
         """
 
         self.db.door_attributes = self.db.reset_door_attributes
+        
+    def at_traverse(self, traversing_object, target_location, **kwargs):
+        """
+        This implements the actual traversal. The traverse lock has
+        already been checked (in the Exit command) at this point.
+
+        Args:
+            traversing_object (Object): Object traversing us.
+            target_location (Object): Where target is going.
+            **kwargs (dict): Arbitrary, optional arguments for users
+                overriding the call (unused by default).
+
+        """
+        source_location = traversing_object.location
+        
+        terrain = source_location.db.terrain
+        
+        if terrain:
+            if terrain == "inside":
+                terrain_cost = 0
+            elif terrain == "city":
+                terrain_cost = 1
+            elif terrain == "field":
+                terrain_cost = 2
+            elif terrain == "forest":
+                terrain_cost = 3
+            elif terrain == "hills":
+                terrain_cost = 4
+            elif terrain == "mountain":
+                terrain_cost = 5
+            elif terrain == "water swim":
+                terrain_cost = 6
+            elif terrain == "swamp":
+                terrain_cost = 7
+            elif terrain == "underwater":
+                terrain_cost = 8
+            elif terrain == "air":
+                terrain_cost = 9
+            elif terrain == "desert":
+                terrain_cost = 10
+            elif terrain == "vacuum":
+                terrain_cost = 11
+            elif terrain == "max":
+                terrain_cost = 12
+            else:
+                terrain_cost = 0
+        
+        if terrain cost > traversing_object.moves_current:
+            traversing.obj.msg("You are too tired to move further.")
+            return
+        
+        if traversing_object.move_to(target_location):
+            traversing_object.moves_spent += terrain_cost
+            self.at_after_traverse(traversing_object, source_location)
+        else:
+            if self.db.err_traverse:
+                # if exit has a better error message, let's use it.
+                traversing_object.msg(self.db.err_traverse)
+            else:
+                # No shorthand error message. Call hook.
+                self.at_failed_traverse(traversing_object)
+
