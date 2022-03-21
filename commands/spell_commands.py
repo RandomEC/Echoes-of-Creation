1e852ac4cb4655ea1e0dd9f69c15ad4b7801d937
RandomEC <97635743+RandomEC@users.noreply.github.com>
2022-03-21 12:04:35 -0400

    Update spell_commands.py
    Added CmdCreateWater

--- commands/spell_commands.py
+++ commands/spell_commands.py
@@ -38,3 +38,58 @@ class CmdCreateFood(MuxCommand):
             return
 
         rules_magic.do_create_food(caster, cost)
+       
+class CmdCreateWater(MuxCommand):
+    """
+    Create water in a drink container in your possession.
+    Usage:
+      cast create water <drink container>
+      create water <drink container>
+    Creates water in a drink container in your possession.
+    """
+
+    key = "create water"
+    aliases = ["cast create water"]
+    locks = "cmd:all()"
+    arg_regex = r"\s|$"
+
+    spell = rules_skills.get_skill(key)
+    wait_state = spell["wait state"]
+
+    def func(self):
+        """Implement create water"""
+
+        caster = self.caller
+
+        # Check whether anything about the room or affects on the caster
+        # would prevent casting. Check_cast returns output for the state
+        # if true, False if not.
+        if rules_magic.check_cast(caster):
+            caster.msg(check_cast(caster))
+            return
+
+        if not self.args:
+            caster.msg("Create water where?")
+            return
+        
+        target_container = caster.search(self.args, location=caster)
+        
+        if not target_container:
+            caster.msg("You have no %s." self.args)
+            return
+        
+        if target_container.db.item_type != "drink container":
+            caster.msg("%s is not able to hold water." % (target_container.key[0].upper() + target_container.key[1:]))
+            return
+        
+        if target_container.db.liquid_type != "water":
+            caster.msg("%s already contains some of another liquid." % (target_container.key[0].upper() + target_container.key[1:]))
+            return
+                
+        cost = rules_magic.mana_cost(spell)
+
+        if caster.mana_current < cost:
+            caster.msg("You do not have sufficient mana to cast create water!")
+            return
+
+        rules_magic.do_create_water(caster, cost, target_container)
