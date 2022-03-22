34c8f6680bdd55aa9a945e1bc851beb9e43afe20
RandomEC <97635743+RandomEC@users.noreply.github.com>
2022-03-22 12:03:40 -0400

    Update command.py
    Added disintegrate timer removal on sacrifice.

--- commands/command.py
+++ commands/command.py
@@ -1066,6 +1066,7 @@ class CmdSacrifice(MuxCommand):
         if obj.db.item_type == "food":
             caller.msg("The gods appreciate your addition to their feast table.")
             obj.location = None
+            rules.remove_disintegrate_timer(obj)
             caller.location.msg_contents("%s sacrifices %s to the gods."
                                          % (caller.name, obj.name),
                                          exclude=caller)
@@ -1116,6 +1117,7 @@ class CmdSacrifice(MuxCommand):
             caller.msg("The gods award you %d %s for your sacrifice." % (moves_award, award))
 
         obj.location = None
+        remove_disintegrate_timer(obj)
         caller.location.msg_contents("%s sacrifices %s to the gods."
                                          % (caller.name, obj.name),
                                          exclude=caller)
