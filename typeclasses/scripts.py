34be248468e43f68236f31cb97bc2472404e6d92
RandomEC <brad.maynes@gmail.com>
2022-04-18 18:53:11 -0400

    bug fixing

--- typeclasses/scripts.py
+++ typeclasses/scripts.py
@@ -206,12 +206,11 @@ class MobileMovementScript(Script):
 
                                     Random.msg("%s" % destination.tags.all())
 
-                                    area_tag = area + ":area name"
-                                    if area_tag in destination.tags.all():
+                                    if area in destination.tags.all():
                                         if destination.db.room_flags:
                                             if "no mob" not in destination.db.room_flags:
                                                 if ("solitary" not in destination.db.room_flags and "private" not in destination.db.room_flags) or mobile.home == destination:
-                                                    if area_tag in destination.tags.all():
+                                                    if area in destination.tags.all():
                                                         mobile.move_to(destination)
                                         else:
                                             mobile.move_to(destination)
